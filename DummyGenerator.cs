45272fde0246cffd6d94961191ea11ac46db607e
Ga gang <wdmab1204@naver.com>
2024-11-20 16:03:41 +0900

    구조체 파싱

--- DummyGenerator.cs
+++ DummyGenerator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Reflection;
 
 namespace ByteConverter
 {
@@ -22,11 +23,11 @@ namespace ByteConverter
             var items = new List<ItemData>();
             var random = new Random();
 
-            for (int i = 0; i < 100; i++)
+            for (int i = 0; i < 10; i++)
             {
                 items.Add(new ItemData
                 {
-                    timeStamp = random.Next(1404715000, 1404716000),
+                    timeStamp = i + 1,
                     dbID = random.Next(10000, 30000),
                     userDbId = random.Next(),
                     templateId = random.Next(1000, 4000),
@@ -56,67 +57,85 @@ namespace ByteConverter
                 }
             }
 
-            GenerateBin(items);
-            PrintBinFile();
+            GenerateBin(columnTypes, items);
+            PrintBinFile<ItemData>();
             System.Diagnostics.Process.Start(workspace);
         }
 
-        public static void GenerateBin(List<ItemData> items)
+        public static unsafe void GenerateBin<T>(byte[] columnTypes, List<T> items) where T : struct
         {
             var binPath = Path.Combine(workspace, "dummy.bin");
             using (var fileStream = new FileStream(binPath, FileMode.Create, FileAccess.Write, FileShare.None))
             using (var writer = new BinaryWriter(fileStream))
             {
                 //컬럼 개수 저장(int)
-                writer.Write(5);
+                writer.Write(columnTypes.Length);
 
                 //컬럼 별 자료형 저장
-                writer.Write((byte)Define.ColumnType.Int32);
-                writer.Write((byte)Define.ColumnType.Int64);
-                writer.Write((byte)Define.ColumnType.Int64);
-                writer.Write((byte)Define.ColumnType.Int32);
-                writer.Write((byte)Define.ColumnType.Int32);
+                for (int i = 0; i < columnTypes.Length; i++)
+                    writer.Write(columnTypes[i]);
 
                 foreach (var item in items)
                 {
-                    writer.Write(item.timeStamp);
-                    writer.Write(item.dbID);
-                    writer.Write(item.userDbId);
-                    writer.Write(item.templateId);
-                    writer.Write(item.amount);
+                    T* dataAddress = &item;
+                    int offset = 0;
+                    for (int i = 0; i < columnTypes.Length; i++)
+                    {
+                        if (columnTypes[i] == (byte)Define.ColumnType.Int32)
+                        {
+                            writer.Write(*(int*)(dataAddress + offset));
+                            offset += 4;
+                        }
+                        else if (columnTypes[i] == (byte)Define.ColumnType.Int64)
+                        {
+                            writer.Write(*(long*)(dataAddress + offset));
+                            offset += 8;
+                        }
+                    }
                 }
             }
         }
 
-        public static void PrintBinFile()
+        public static unsafe void PrintBinFile<T>() where T : struct
         {
             Console.WriteLine("TimeStamp\tDbId\tUserDbId\tTemplateId\tAmount");
 
+            List<T> list = new List<T>();
             var binPath = Path.Combine(workspace, "dummy.bin");
+            
+
             using (var fileStream = File.OpenRead(binPath))
             using (var reader = new BinaryReader(fileStream))
             {
+                //컬럼 개수
                 int columnCount = reader.ReadInt32();
                 Define.ColumnType[] columnTypes = new Define.ColumnType[columnCount];
-                for(int i=0; i < columnCount; i++)
+                for (int i = 0; i < columnCount; i++)
+                    //각 컬럼 타입들 
                     columnTypes[i] = (Define.ColumnType)reader.ReadByte();
 
                 int row = 0;
+                FieldInfo[] fields = typeof(T).GetFields();
                 while (fileStream.Position < fileStream.Length)
                 {
-                    Console.Write($"{GetValue(columnTypes[row++ % columnCount])}\t");
-                    if (row % columnCount == 0)
-                        Console.WriteLine();
-                }
-
-                string GetValue(Define.ColumnType type)
-                {
-                    if (type == Define.ColumnType.Int32)
-                        return reader.ReadInt32().ToString();
-                    else if (type == Define.ColumnType.Int64)
-                        return reader.ReadInt64().ToString();
-                    else
-                        return "-1";
+                    T data = new T();
+                    T* pData = &data;
+                    byte* dataAddress = (byte*)pData;
+                    for (int i=0; i< columnCount; i++)
+                    {
+                        if (columnTypes[i] == Define.ColumnType.Int32)
+                        {
+                            *(int*)(dataAddress) = reader.ReadInt32();
+                            dataAddress += 4;
+                        } 
+                        else if (columnTypes[i] == Define.ColumnType.Int64)
+                        {
+                            *(long*)(dataAddress) = reader.ReadInt64();
+                            dataAddress += 8;
+                        }
+                        row++;
+                    }
+                    list.Add(data);
                 }
             }
         }
